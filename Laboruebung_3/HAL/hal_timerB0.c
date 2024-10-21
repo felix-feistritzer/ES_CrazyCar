d16930b229d7586ce51422da2daa8842ee2338f7
Felix Feistritzer <felix.feistritzer@edu.fh-joanneum.at>
2024-10-21 13:16:29 +0200

    Laboruebung 3: es blinkt!!!

--- Laboruebung_3/HAL/hal_timerB0.c
+++ Laboruebung_3/HAL/hal_timerB0.c
@@ -3,20 +3,30 @@
 
 void HAL_TimerB0_Init(void)
 {
-    TB0CTL |= TBSSEL__SMCLK;    // Timer_B clock source set to SMCLK
-    TB0CTL |= ID__8;            // Input divider set to /8
-    TB0EX0 |= TBIDEX__4;        // Input divider expansion set to /4
+    TB0CTL &= ~MC_3;
     TB0CTL |= MC__UP;           // Mode control set to Up mode
-    TB0CTL |= TBIE;             // Timer_B interrupt enable
 
-    TB0CCR0 |= 0x9896;           // 2.5 GHz /2 /32 => 2Hz
+    TB0CTL &= ~TBSSEL_3;
+    TB0CTL |= TBSSEL__SMCLK;    // Timer_B clock source set to SMCLK
 
-    TB0CCTL0 |= CM_1;            // Capture mode: Capture on rising edge
-    TB0CCTL0 &= ~CAP;            // Capture mode: Compare mode
-    TB0CCTL0 |= CCIE;            // Capture/Compare interrupt enabled
+    TB0CTL &= ~ID_3;
+    TB0CTL |= ID__8;            // Input divider set to /8
 
-    TB0CCTL0 &= ~CCIFG;          // Clear interrupt flags
-    TB0CTL &= ~TBIFG;
+    TB0EX0 &= ~TBIDEX_7;
+    TB0EX0 |= TBIDEX__4;        // Input divider expansion set to /4
+
+    TB0CCTL0 &= ~CCIFG;         // Clear interrupt flags
+    TB0CCTL0 &= ~COV;           // Capture overflow: no capture overflow occurred
+    TB0CCTL0 |= CCIE;           // Capture/Compare interrupt enabled
+    TB0CCTL0 &= ~CAP;           // Capture mode: Compare mode
+
+    TB0CCTL0 &= ~CM_3;
+    TB0CCTL0 |= CM_1;           // Capture mode: Capture on rising edge
+
+    TB0CCR0 &= ~0xFFFF;
+    TB0CCR0 |= 0x9896;          // 2.5 GHz /2 /32 => 2Hz
+
+    // TB0CTL &= ~TBIFG;
 }
 
 // Interrupts
@@ -27,5 +37,5 @@ __interrupt void TimerB0(void)
      LCD_BL_TOGGLE;          // Toggle LCD_BL
 
      TB0CCTL0 &= ~CCIFG;     // Clear interrupt flag
-     TB0CTL &= ~TBIFG;
+     // TB0CTL &= ~TBIFG;
 }
--- Laboruebung_3/HAL/hal_ucs.c
+++ Laboruebung_3/HAL/hal_ucs.c
@@ -13,7 +13,7 @@ void HAL_UCS_Init(void)
     }
 
     UCSCTL6 &= ~XT2DRIVE_1;     // XT2DRIVE set to current consumption 16 MHz to 24 MHz
-    UCSCTL4 |= SELM__XT2CLK;    // SMCLK source set to XT2CLK
+    UCSCTL4 |= SELS__XT2CLK;    // SMCLK source set to XT2CLK
     UCSCTL4 |= SELM__XT2CLK;    // MCLK source set to XT2CLK
     UCSCTL5 |= DIVS__8;         // SMCLK source divider 1/8 => 2.5 MHz
 
